e060fccb957c7710040d6da7519cce172a8d9454
gwanghoyu <poiuy5958@dreamage.com>
2022-12-21 16:49:34 +0900

    add conversion header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <header>
             <a href="d-day">디데이 계산기</a>
             <a href="loan-calc">대출이자 계산기</a>
+            <a href="conversion">단위변환기</a>
         </header>
     )
 }
